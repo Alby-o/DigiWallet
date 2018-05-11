5092777c352c7907e037ce28a9f57c2bd47d44b5
Alexander Blyth <alexanderblyth5@gmail.com>
2018-05-11 17:58:56 +1000

    Added home screen

--- js/script.js
+++ js/script.js
@@ -1,12 +1,17 @@
 $(document).ready(function() {
-  $("#submit").click(function() {
+  $("#login-form").submit(function(event) {
     var email = $("#email").val();
     var password = $("#password").val();
+
     if(email == "someone@email.com" && password == "password") {
       $(".feedback").removeClass("fail").addClass("success").text("Login successful");
+      setTimeout(function(){
+        window.location = "home.html";        
+      }, 1000);
     } else {
       $(".feedback").removeClass("success").addClass("fail").text("Username or password incorrect");
     }
-    // alert("button clicked");
+
+    event.preventDefault();
   });
 });
